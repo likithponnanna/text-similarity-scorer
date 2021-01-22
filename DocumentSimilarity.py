cd8db61824a7fffdb07c5af19636c14c5cd2e871
Likith Ponnanna <likith.april13@gmail.com>
2021-01-22 00:02:08 -0800

    Server and command line tool completed

--- DocumentSimilarity.py
+++ DocumentSimilarity.py
@@ -2,87 +2,73 @@ from constants import stop_word_list
 import math
 import string
 
+
 class DocumentSimilarity:
-  def __init__(self):
-    self.stop_words = stop_word_list
+    def __init__(self):
+        self.stop_words = stop_word_list
 
-  def preprocess_text(self, text):
-    ''' Function replaces punctuations with spaces removes special 
-    characters, converts the string to lowercase and returns a list 
-    of words.'''
-    chars = "\\`*_{}[]()>#+-.!$','"
-    for c in chars:
-        if c == "'": 
-            text = text.replace(c, " ")
-        else:
-            text = text.replace(c, "")
-    return text.lower().split()
+    def preprocess_text(self, text):
+        """Function replaces punctuations with spaces removes special
+        characters, converts the string to lowercase and returns a list
+        of words."""
+        chars = "\\`*_{}[]()>#+-.!$','"
+        for c in chars:
+            if c == "'":
+                text = text.replace(c, " ")
+            else:
+                text = text.replace(c, "")
+        return text.lower().split()
 
-  def remove_stop_words(self, word_list):
-    ''' Removes stop words from the word list that is generated.'''
-    return  [x for x in word_list if x not in self.stop_words]
+    def remove_stop_words(self, word_list):
+        """ Removes stop words from the word list that is generated."""
+        return [x for x in word_list if x not in self.stop_words]
 
-  def dictionary_dot_product(self, sorted_word_dict_one, sorted_word_dict_second):
-    ''' Compute dot product'''    
-    return sum(sorted_word_dict_one[key]*sorted_word_dict_second.get(key, 0) for key in sorted_word_dict_one)
+    def dictionary_dot_product(self, sorted_word_dict_one, sorted_word_dict_second):
+        """ Compute dot product"""
+        return sum(
+            sorted_word_dict_one[key] * sorted_word_dict_second.get(key, 0)
+            for key in sorted_word_dict_one
+        )
 
-  def word_frequency (self, word_list):
-    ''' Generates word frequency dictionary'''  
-    count_dict = dict()
-    for items in word_list: 
-            count_dict[items] = word_list.count(items)     
-    return count_dict 
+    def word_frequency(self, word_list):
+        """ Generates word frequency dictionary"""
+        count_dict = dict()
+        for items in word_list:
+            count_dict[items] = word_list.count(items)
+        return count_dict
 
-  
-  def string_tokenizer(self, text):
-    ''' Generates final tokenized word frequency map'''    
-    line_list = text
-    word_list = self.preprocess_text(line_list) 
-    stop_word_removed_list = self.remove_stop_words(word_list)
-    freq_mapping = self.word_frequency(stop_word_removed_list) 
-    print(len(line_list), "lines, ", ) 
-    print(len(word_list), "words, ", ) 
-    print(len(freq_mapping), "distinct words") 
-    return freq_mapping 
+    def string_tokenizer(self, text):
+        """ Generates final tokenized word frequency map"""
+        word_list = self.preprocess_text(text)
+        stop_word_removed_list = self.remove_stop_words(word_list)
+        freq_mapping = self.word_frequency(stop_word_removed_list)
+        return freq_mapping
 
-  def cosine_similarity(self, sorted_word_dict_one, sorted_word_dict_second):
-    '''Computes cosine similarity between two text inputs'''  
-    numerator = self.dictionary_dot_product(sorted_word_dict_one, sorted_word_dict_second) 
-    denominator = math.sqrt(self.dictionary_dot_product(sorted_word_dict_one,
-      sorted_word_dict_one)* self.dictionary_dot_product(sorted_word_dict_second, sorted_word_dict_second)) 
-    return math.acos(numerator / denominator) 
+    def cosine_similarity(self, sorted_word_dict_one, sorted_word_dict_second):
+        """Computes cosine similarity between two text inputs"""
+        numerator = self.dictionary_dot_product(
+            sorted_word_dict_one, sorted_word_dict_second
+        )
+        denominator = math.sqrt(
+            self.dictionary_dot_product(sorted_word_dict_one, sorted_word_dict_one)
+            * self.dictionary_dot_product(
+                sorted_word_dict_second, sorted_word_dict_second
+            )
+        )
 
-  def similarity_score(self, text_first, text_second):
-    '''Function generates a similarity score between two text inputs''' 
-    sorted_word_dict_one = self.string_tokenizer(text_first) 
-    sorted_word_dict_second = self.string_tokenizer(text_second)
-    # print(sorted_word_dict_one)
-    # print(sorted_word_dict_second)
-    distance = 1 - (self.cosine_similarity(sorted_word_dict_one, sorted_word_dict_second) / math.acos(0))
-    print("The similarity score between the provided text snippets is: {:0.4f} ".format(distance)) 
+        try:
+            cos_sim = math.acos(numerator / denominator)
+        except:
+            cos_sim = math.acos(0)
 
+        return cos_sim
 
-
-# text_1 = "The easiest way to earn points with Fetch Rewards is to just shop for the products " \
-# "you already love. If you have any participating brands on your receipt, you'll get points " \
-# "based on the cost of the products. You don't need to clip any coupons or scan individual "\
-# "barcodes. Just scan each grocery receipt after you shop and we'll find the savings for you."
-
-# text_2 = "The easiest way to earn points with Fetch Rewards is to just shop for the items you " \
-# "already buy. If you have any eligible brands on your receipt, you will get " \
-# "points based on the total cost of the products. You do not need to cut out " \
-# "any coupons or scan individual UPCs. Just scan your receipt after you check " \
-# "out and we will find the savings for you."
-
-# text_3 = "We are always looking for opportunities for you to earn more points, which is why " \
-# "we also give you a selection of Special Offers. These Special Offers are opportunities to earn " \
-# "bonus points on top of the regular points you earn every time you purchase a participating brand. " \
-# "No need to pre-select these offers, we'll give you the points whether or not you knew about the offer. " \
-# "We just think it is easier that way."
-
-
-# docObj = DocumentSimilarity()
-# docObj.similarity_score(text_1, text_2)
-# docObj.similarity_score(text_1, text_3)
-# docObj.similarity_score("aa qq", "qq qq")
-# docObj.similarity_score("aa qq", "qq tt")
+    def similarity_score(self, text_first, text_second):
+        """Function generates a similarity score between two text inputs"""
+        sorted_word_dict_one = self.string_tokenizer(text_first)
+        sorted_word_dict_second = self.string_tokenizer(text_second)
+        sim_score = 1 - (
+            self.cosine_similarity(sorted_word_dict_one, sorted_word_dict_second)
+            / math.acos(0)
+        )
+        return "{:0.4f}".format(sim_score)
--- server.py
+++ server.py
@@ -1,4 +1,48 @@
-from DocumentSimilarity import DocumentSimilarity as docSim 
-from flask import Flask, redirect, url_for, render_template
+from DocumentSimilarity import DocumentSimilarity as docSim
+from flask import Flask, redirect, url_for, render_template, request, jsonify
+import math
 
-print("Done")
+app = Flask(__name__)
+
+
+result = -1
+text_one = ""
+text_two = ""
+docSim_Obj = docSim()
+
+
+@app.route("/", methods=["POST", "GET", "DELETE"])
+def homepage():
+    """Handles requests to get text inputs from webpage and
+    calculates similarity score and appropriately redirects user"""
+    if request.method == "POST":
+        text_one = request.form["text_one"]
+        text_two = request.form["text_two"]
+        print("T1: ", text_one, " T2: ", text_two)
+
+        result = docSim_Obj.similarity_score(text_one, text_two)
+        return render_template(
+            "output.html", result=result, text_one=text_one, text_two=text_two
+        )
+    else:
+        return render_template("index.html")
+
+
+@app.route("/api/similarity", methods=["POST"])
+def similarity_api():
+    """Provides an API endpoint to get text inputs from a json body
+    and returns similarity score in JSON fromat"""
+    if request.method == "POST":
+
+        data = request.get_json()
+
+        text_one = data["text_one"]
+        text_two = data["text_two"]
+
+        result = docSim_Obj.similarity_score(text_one, text_two)
+
+        return jsonify({"similarity_score": result})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
